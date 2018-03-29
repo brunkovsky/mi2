849a7e2f7db0f2a0edbeeacc4c3db456a4c2c0de
brunkovskyi <brunkovskyi@ngoar.net>
2018-03-29 14:43:32 +0300

    commit1

--- internalmodule/src/main/java/tags/MyLangUtility.java
+++ internalmodule/src/main/java/tags/MyLangUtility.java
@@ -8,31 +8,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MyLangUtility {
-    public static String getExchangeRage() throws IOException, JSONException {
-
-        String url = "https://api.privatbank.ua/p24api/exchange_rates?json&date=01.12.2014 ";
-
-        URL obj = new URL(url);
-        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
-
-        connection.setRequestMethod("GET");
-
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+    public static String getExchangeRage(String currency) throws Exception {
+        String response= getResponse(currency);
+        response = response.substring(1, response.length() - 1);
+        JSONObject jsonObject = new JSONObject(response);
+        return jsonObject.get("rate").toString();
+    }
+    private static String getResponse(String valuta) throws Exception {
+        Date dateNow = new Date();
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("yyyyMMdd");
+        String today = formatForDateNow.format(dateNow);
+        String url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=" + valuta + "&date=" + today + "&json";
+        HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+        int responseCode = con.getResponseCode();
+        if (responseCode != 200) {
+            throw new Exception("responseCode = " + Integer.toString(responseCode));
+        }
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
         String inputLine;
-        StringBuffer response = new StringBuffer();
-
+        StringBuilder response = new StringBuilder();
         while ((inputLine = in.readLine()) != null) {
             response.append(inputLine);
         }
         in.close();
-
-        JSONObject json = new JSONObject(response.toString());
-        Object exchangeRate = json.get("exchangeRate");
-
-
-        return response.toString();
+        con.disconnect();
+        return(response.toString());
     }
 }
